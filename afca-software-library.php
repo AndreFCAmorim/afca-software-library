74ae46249ee2358d3568078ba180ddb140862635
André Amorim <andrefcamorim@gmail.com>
2023-06-25 01:14:11 +0100

    Move code from main file to init

--- afca-software-library.php
+++ afca-software-library.php
@@ -2,9 +2,10 @@
 /**
  * Plugin Name:       WP Software Library
  * Description:       This plugin allows you to manage the information of all of your themes and plugins. Everything is accessible trough the WordPress API.
+ * Plugin URI:        https://andreamorim.site/plugin-documentation/sofware-library/
  * Requires at least: 6.1
  * Requires PHP:      7.4
- * Version:           0.2
+ * Version:           1.0
  * Author:            André Amorim
  * Author URI:        https://andreamorim.site
  * Text Domain:       afca-software-library
@@ -17,42 +18,5 @@ if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
-/**
- * Load plugin text domain for translations.
- */
-function afca_load_plugin_language() {
-	// Replace 'your-textdomain' with your plugin's text domain.
-	load_plugin_textdomain( 'afca-software-library', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-}
-add_action( 'plugins_loaded', 'afca_load_plugin_language' );
-
-/**
- * Register the custom post type
- */
-use Afca\Plugins\SoftwareLibrary\PostType;
-new PostType();
-
-/**
- * Register acf meta fields
- */
-use Afca\Plugins\SoftwareLibrary\MetaFields;
-new MetaFields(
-	plugin_dir_path( __FILE__ ) . '/libs/acf/',
-	plugin_dir_url( __FILE__ ) . '/libs/acf/',
-);
-
-/**
- * Register endpoints
- */
-use Afca\Plugins\SoftwareLibrary\Endpoints;
-new Endpoints();
-
-/**
- * Register updates
- */
-use Afca\Plugins\SoftwareLibrary\Updates;
-$plugin_dir  = plugin_dir_path( __FILE__ );
-$plugin_data = get_plugin_data( $plugin_dir . 'afca-software-library.php' );
-$update_class = new Updates( 'https://andreamorim.site/', basename( $plugin_dir ), $plugin_data['Version'] );
-
-add_action( 'init', [ $update_class, 'check_for_updates_on_hub' ] );
+use Afca\Plugins\SoftwareLibrary\Init;
+new Init( plugin_dir_path( __FILE__ ), plugin_dir_url( __FILE__ ) );
---
+++ src/Init.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Afca\Plugins\SoftwareLibrary;
+
+use Afca\Plugins\SoftwareLibrary\PostType;
+use Afca\Plugins\SoftwareLibrary\MetaFields;
+use Afca\Plugins\SoftwareLibrary\Endpoints;
+use Afca\Plugins\SoftwareLibrary\Updates;
+
+class Init {
+
+	private string $plugin_dir_path;
+	private string $plugin_dir_url;
+
+	public function __construct( string $plugin_dir_path, string $plugin_dir_url ) {
+		$this->plugin_dir_path = $plugin_dir_path;
+		$this->plugin_dir_url  = $plugin_dir_url;
+
+		/**
+		 * Load plugin text domain for translations.
+		 */
+		add_action( 'plugins_loaded', [ $this, 'afca_load_plugin_language' ] );
+
+		/**
+		 * Register the custom post type
+		 */
+		new PostType();
+
+		/**
+		 * Register acf meta fields
+		 */
+		new MetaFields(
+			$this->plugin_dir_path . '/libs/acf/',
+			$this->plugin_dir_url . '/libs/acf/',
+		);
+
+		/**
+		 * Register endpoints
+		 */
+		new Endpoints();
+
+		/**
+		 * Register updates
+		 */
+		$plugin_dir   = $this->plugin_dir_path;
+		$plugin_data  = get_plugin_data( $plugin_dir . 'afca-software-library.php' );
+		$update_class = new Updates( 'https://andreamorim.site/', basename( $plugin_dir ), $plugin_data['Version'] );
+
+		add_action( 'init', [ $update_class, 'check_for_updates_on_hub' ] );
+	}
+
+
+	public function afca_load_plugin_language() {
+		// Replace 'your-textdomain' with your plugin's text domain.
+		load_plugin_textdomain( 'afca-software-library', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+}
